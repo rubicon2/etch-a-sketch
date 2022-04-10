f73aa4a92ffa59d6bd25535ddd0187df7d85e564
Liam <liargent@hotmail.com>
2022-04-10 22:31:13 +0100

    Add sketch reset, add resolution prompt, change layout

--- code.js
+++ code.js
@@ -1,5 +1,11 @@
 const sketchpad = document.getElementById("sketchpad");
-const colorInfo = document.getElementById("currentColor");
+const rSlider = document.getElementById("r-slider");
+const gSlider = document.getElementById("g-slider");
+const bSlider = document.getElementById("b-slider");
+const colorPreview = document.getElementById("color-preview");
+const resetMessage = "Set resolution (8 - 100):";
+const minResolution = 8;
+const maxResolution = 100;
 let currentRGB = "rgb(255, 255, 255)";
 
 function initSketchpad() {
@@ -38,13 +44,18 @@ function avgRGB(color1, color2) {
     return avgRgb;
 }
 
-function RGBToCss(rgb) {
-    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+// function RGBToCss(rgb) {
+//     return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+// }
+
+function RGBToCss(r, g, b) {
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 function paintElement() {
     let elementColor = this.currentColor;
-    this.currentColor = RGBToCss(avgRGB(elementColor, currentRGB))
+    let newColor = avgRGB(elementColor, currentRGB);
+    this.currentColor = RGBToCss(newColor[0], newColor[1], newColor[2]);
     this.style.backgroundColor = this.currentColor; 
 }
 
@@ -58,12 +69,34 @@ function createDiv(sizeAsPercentageOfGrid) {
     return newDiv;
 }
 
+function clearDivsGrid() {
+    let divs = document.querySelectorAll("#sketchpad > div");
+    for(let i = 0; i < divs.length; i++) {
+        divs[i].remove();
+    }
+}
+
 function createDivsGrid(rows) {
+    if (rows < minResolution) 
+        rows = minResolution;
+    else if (rows > maxResolution) {
+        rows = maxResolution;
+    }
     let totalDivs = rows * rows;
     let sizeAsPercentageOfGrid = 100 / rows;
     for(let i = 0; i < totalDivs; i++) {
         sketchpad.appendChild(createDiv(sizeAsPercentageOfGrid));
     }
+}
+
+function resetGrid() {
+    clearDivsGrid();
+    createDivsGrid(prompt(resetMessage, 16));
+}
+
+function setRGB() {
+    currentRGB = RGBToCss(rSlider.value, gSlider.value, bSlider.value);
+    colorPreview.style.backgroundColor = currentRGB;
 }
 
 function randomiseColor() {
@@ -72,13 +105,15 @@ function randomiseColor() {
         Math.floor(Math.random() * 255), 
         Math.floor(Math.random() * 255), 
     ]
-    currentRGB = RGBToCss(randomRGB);
-    colorInfo.style.color = currentRGB;
-    colorInfo.textContent = currentRGB;
-
+    currentRGB = RGBToCss(randomRGB[0], randomRGB[1], randomRGB[2]);
 }
 
 window.addEventListener("resize", initSketchpad);
-window.addEventListener("click", randomiseColor);
+
+rSlider.oninput = setRGB;
+gSlider.oninput = setRGB;
+bSlider.oninput = setRGB;
+
 initSketchpad();
-createDivsGrid(16);
+setRGB();
+createDivsGrid(prompt(resetMessage, 16));
